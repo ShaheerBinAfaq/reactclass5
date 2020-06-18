4da246340635a2bc6495ad77a45398d46484e332
ShaheerBinAfaq <shaheerafaq@gmail.com>
2020-06-18 11:02:48 +0500

    update num through context

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import Parent from './parent';
 import ValueContext from './ValueContext';
 
 function App() {  
-  let value = 81;
+  let value = useState(81);
   return (
     <ValueContext.Provider value={value}>
     <div className="App">
       Hello World
-      <Parent></Parent>            
+      <Parent></Parent>
+      <button onClick={()=>{value[1](++value[0])}}>update num</button>
     </div>
     </ValueContext.Provider>
   );
